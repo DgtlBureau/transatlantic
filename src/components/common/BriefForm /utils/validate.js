48cd7f79095c390a6ad54ab73c079e369825c32a
Daria_Ikon <ikonnikova.daria@gmail.com>
2023-04-12 14:51:17 +0700

    refactor: refactor validate, add disabled on btn, add modal with content

--- src/components/common/BriefForm /utils/validate.js
+++ src/components/common/BriefForm /utils/validate.js
@@ -1,22 +1,17 @@
 export const validate = (values) => {
   const errors = {};
 
-  const schemeName = /^[А-ЯЁ][а-яё]+$/i.test;
-  const schemeTel = /^((8|\+7)[ - ]?)?(\(?\d{3,4}\)?[ - ]?)?[\d\- ]{5,10}$/i
-    .test;
-  const schemeMail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test;
-
   if (!values.email) {
     errors.email = "заполните поле";
-  } else if (!schemeMail(values.email)) {
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
     errors.email = "некорректный email";
   }
 
   if (!values.name) {
     errors.name = "введите имя";
-  } else if (!schemeName(values.name)) {
+  } else if (!/^[А-Яа-я]+$/i.test(values.name)) {
     errors.name = "введите корректное имя";
-  } else if (values.name.length <= 3) {
+  } else if (values.name.length < 3) {
     errors.name = "не менее трех символов";
   } else if (values.name.length >= 30) {
     errors.name = "не более тридцати символов";
@@ -24,7 +19,9 @@ export const validate = (values) => {
 
   if (!values.tel) {
     errors.tel = "заполните поле";
-  } else if (!schemeTel(values.tel)) {
+  } else if (
+    !/^((8|\+7)[ - ]?)?(\(?\d{3,4}\)?[ - ]?)?[\d\- ]{6,11}$/i.test(values.tel)
+  ) {
     errors.tel = "некорректный номер телефона";
   } else if (values.tel.length > 11) {
     errors.tel = "некорректный номер телефона";
